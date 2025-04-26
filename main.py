e78c7a622a4682abf6d3fcea693d0fa42f375c1a
michaelyusak <michaelyusakt@gmail.com>
2025-04-26 22:15:42 +0700

    add .env

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import os
 import logging
+from dotenv import load_dotenv
 
 from logs.setup import setup_logger
 from crawl.fetcher import fetch_page
+from crawl.parser import extract_links
+from crawl.utils import is_valid_url
+from crawl.storage import save_page
 
 SEEDS_FILE = "configs/seeds.txt"
 
@@ -15,13 +20,17 @@ def load_seeds(filename):
     return seeds
 
 def main():
+    load_dotenv()
+
     setup_logger()
 
     seeds = load_seeds(SEEDS_FILE)
     seen_urls = set()
     url_queue = seeds.copy()
 
-    while url_queue and len(seen_urls) < 10000:
+    site_limit = int(os.getenv("SITE_LIMIT"))
+
+    while url_queue and len(seen_urls) < site_limit:
         url = url_queue.pop(0)
         if url in seen_urls:
             continue
@@ -32,9 +41,14 @@ def main():
             logging.warning(f'failed to fetch {url}')
             continue
 
-        print(html)
+        save_page(url, html)
+        seen_urls.add(url)
 
-    
+        links = extract_links(html, url)
+
+        for link in links:
+            if is_valid_url(link) and link not in seen_urls:
+                url_queue.append(link)
 
 if __name__ == '__main__':
     main()
