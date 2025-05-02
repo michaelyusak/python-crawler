c2e997e1d2f3cf81508a2f34d5b48f97cde18c04
michaelyusak <michaelyusakt@gmail.com>
2025-05-02 14:35:03 +0700

    refactor crawler

---
+++ crawler.py
@@ -0,0 +1,72 @@
+import os
+import time
+import logging
+
+from storage.elastic import ElasticSearchClient
+from storage.postgres import PostgresClient
+from utils.common import hash, is_valid_url
+from utils.parser import html_to_es_doc, extract_links
+from utils.fetcher import fetch_page
+from utils.robots import safe_read_robots_txt
+from storage.local import save_page
+
+class Crawler:
+    def __init__(self, es_client: ElasticSearchClient, pg_client: PostgresClient, seeds: list[str]):
+        self.es_client = es_client
+        self.pg_client = pg_client
+
+        self.seeds = seeds
+        self.seen_urls = set()
+        self.url_queue = seeds.copy()
+
+        self.site_limit = int(os.getenv("SITE_LIMIT"))
+
+        self.disable_local_storage = os.getenv("DISABLE_LOCAL_STORAGE").lower() == "true"
+
+        self.user_agent = os.getenv("USER_AGENT")
+
+
+    def crawl(self):
+        while self.url_queue and len(self.seen_urls) < self.site_limit:
+            time.sleep(0.5)
+
+            url = self.url_queue.pop(0)
+            if url in self.seen_urls:
+                continue
+
+            rp = safe_read_robots_txt(url)
+
+            time.sleep(0.5)
+
+            if not rp.can_fetch(self.user_agent, url):
+                continue
+
+            logging.info(f"crawling: {url}")
+            html = fetch_page(url)
+            if html is None:
+                logging.warning(f'failed to fetch {url}')
+                continue
+
+            hash_id = hash(f"{html}:{url}")
+
+            crawled = self.pg_client.get_site_by_hash(hash_id)
+            if crawled is not None:
+                logging.info("Inequal normalized and hashed, %s - %s, saving %s", crawled["hash"], hash_id, url)
+                continue
+
+            doc = html_to_es_doc(url, html)
+            doc["hash"] = hash_id
+
+            if not self.disable_local_storage:
+                save_page(url, html)
+
+            self.pg_client.save_site(url, html, hash_id)
+            self.es_client.save_site(doc)
+
+            self.seen_urls.add(url)
+
+            links = extract_links(html, url)
+
+            for link in links:
+                if is_valid_url(link) and link not in self.seen_urls:
+                    self.url_queue.append(link)
--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-import os
 import logging
 from dotenv import load_dotenv
-import time
 
 from logs.setup import setup_logger
-from utils.fetcher import fetch_page
-from utils.parser import extract_links, html_to_es_doc
-from utils.common import is_valid_url, hash
 from storage.elastic import ElasticSearchClient
 from storage.postgres import PostgresClient
-from storage.local import save_page
-from utils.robots import safe_read_robots_txt
+from crawler import Crawler
 
 SEEDS_FILE = "configs/seeds.txt"
 
@@ -26,8 +20,6 @@ def load_seeds(filename):
 def main():
     load_dotenv(override=True)
 
-    user_agent = os.getenv("USER_AGENT")
-
     setup_logger()
 
     es_client = ElasticSearchClient()
@@ -37,56 +29,9 @@ def main():
     pg_client.connect()
 
     seeds = load_seeds(SEEDS_FILE)
-    seen_urls = set()
-    url_queue = seeds.copy()
 
-    site_limit = int(os.getenv("SITE_LIMIT"))
-
-    disable_local_storage = os.getenv("DISABLE_LOCAL_STORAGE").lower() == "true"
-
-    while url_queue and len(seen_urls) < site_limit:
-        time.sleep(0.5)
-
-        url = url_queue.pop(0)
-        if url in seen_urls:
-            continue
-
-        rp = safe_read_robots_txt(url)
-
-        time.sleep(0.5)
-
-        if not rp.can_fetch(user_agent, url):
-            continue
-
-        logging.info(f"crawling: {url}")
-        html = fetch_page(url)
-        if html is None:
-            logging.warning(f'failed to fetch {url}')
-            continue
-
-        hash_id = hash(f"{html}:{url}")
-
-        crawled = pg_client.get_site_by_hash(hash_id)
-        if crawled is not None:
-            logging.info("Inequal normalized and hashed, %s - %s, saving %s", crawled["hash"], hash_id, url)
-            continue
-
-        doc = html_to_es_doc(url, html)
-        doc["hash"] = hash_id
-
-        if not disable_local_storage:
-            save_page(url, html)
-
-        pg_client.save_site(url, html, hash_id)
-        es_client.save_site(doc)
-
-        seen_urls.add(url)
-
-        links = extract_links(html, url)
-
-        for link in links:
-            if is_valid_url(link) and link not in seen_urls:
-                url_queue.append(link)
+    crawler = Crawler(es_client, pg_client, seeds)
+    crawler.crawl()
 
     es_client.close()
     pg_client.close()
